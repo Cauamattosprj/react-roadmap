99471fb82d3378c4a2dd3671a81cab88a9842eea
Caua Mattos <caua.pereira@flybank.com.br>
2024-10-31 16:59:52 -0300

    first

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Chart = () => {
   return (
-    <div className='bg-[#f3efef] w-[200px] grid grid-cols-2 grid-rows-2 p-4 gap-0'>
+    <div className='bg-[#f3efef] w-[200px] grid grid-cols-2 grid-rows-2 p-4 gap-0 mx-auto rounded-lg'>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png" alt="" className='size-12 row-span-3' />
-        <p>Bitcoin</p>
-        <p>Valor: R$ X</p>
+        <p className='font-bold'>Bitcoin</p>
+        <p className='font-bold'>Valor:</p>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-4 py-4">
+      <div className="max-w-7xl mx-auto px-4 py-4 mb-8">
         <div className="flex justify-between items-center">
           <div className="text-white text-lg font-bold">
             Meu Dashboard
